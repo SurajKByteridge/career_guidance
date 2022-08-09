04b562633bddd2cd20c79f58fa793c34cce8bb9c
SurajkByteridge <surajk@byteridge.com>
2022-08-09 13:15:03 +0530

    homescreen added

--- Backend/src/main/java/com/example/carrerguidance/Service/impl/CareerServiceImpl.java
+++ Backend/src/main/java/com/example/carrerguidance/Service/impl/CareerServiceImpl.java
@@ -8,7 +8,6 @@ import com.example.carrerguidance.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
-;
 
 import java.util.List;
 
